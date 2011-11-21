1e50f842dc543b18903421f4cea7235f1831f92c
M. Scott Ford <scott@corgibytes.com>
2011-11-21 11:20:09 -0500

    Adds base rails add. Needed to run deface.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DefaceHack::Application.config.secret_token = '0054b81b08ae021c172f8506e294735a666dc44b70e507b076c09161c706156ee718832a8a452b24bc5a9c2d43ce6ce011c099aa7f317a96fddc0109e1ab7feb'
